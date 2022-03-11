01672a63f72059a7a5d8d89413fb1941ca27d931
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-03-11 17:01:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+
+
+
+using System;
+
+namespace TPMOD3
+{
+   class Program
+    {
+        static void  Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); 
+        }
+    }
+
+
+}
